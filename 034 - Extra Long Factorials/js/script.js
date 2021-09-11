5c0256f9c2b4ee2d03eb587b2b89a6b5194ddb4b
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2021-09-11 17:01:19 -0300

    Add files via upload

---
+++ 034 - Extra Long Factorials/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function extraLongFactorials(n) {
+  var bigInt = BigInt(n);
+  var factorial = 1n;
+
+  for (let i = 0n; i < bigInt; i++) {
+    factorial *= bigInt - i;
+  }
+
+  console.log(factorial.toString());
+}
+extraLongFactorials(35);
