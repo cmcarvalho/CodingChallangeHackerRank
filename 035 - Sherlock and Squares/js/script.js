a47cb488d298ccfc397b5cde2582ed27bd349a41
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2021-09-13 00:03:19 -0300

    Add files via upload

--- 035 - Sherlock and Squares/js/script.js
+++ 035 - Sherlock and Squares/js/script.js
@@ -3,7 +3,8 @@
 function squares(a, b) {
   // Write your code here
   let sum = 0;
-  let j = Math.ceil(Math.sqrt(a)) + 1;
+  console.log(Math.ceil(Math.sqrt(a)));
+  let j = Math.ceil(Math.sqrt(a));
   console.log(j);
   while ((j + sum) * (j + sum) <= b) {
     sum++;
