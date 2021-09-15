e4e57896b5e36e3f401e8d6a62ca10fa7d01b538
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2021-09-14 23:40:14 -0300

    Add files via upload

---
+++ 033 - Larry's Array - FAILED!/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function larrysArray(A) {
+  // Write your code here
+  for (let i = 0; i < A.length; i++) {
+    console.log("YES");
+  }
+}
+
+larrysArray([1, 2, 3, 4, 5], [1, 5, 8, 6, 2], [1, 3, 5, 6, 3]);
