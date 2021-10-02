dab7b475a878490d389d844aafb2c2459380fea3
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2021-10-02 15:38:47 -0300

    Add files via upload

---
+++ 037 - Equalize the Array - FAILED!/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function equalizeArray(arr) {
+  // Write your code here
+  let equalize = {};
+
+  for (let i = 0; i < arr.length; i++) {}
+}
+
+console.log(equalizeArray([3, 3, 2, 1, 3]));
